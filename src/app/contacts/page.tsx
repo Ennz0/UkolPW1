c7caaa4bc6c202b01c8696c4adcf58dd1939c101
Ennz0 <98810648+Ennz0@users.noreply.github.com>
2025-06-05 18:12:24 +0200

    modified:   src/app/contacts/page.tsx

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -10,7 +10,7 @@ const Contact = () => {
         <div className="text-center mb-12 text-[#c59f61]">
           <h1 className="text-5xl font-bold mb-4">Get in Touch</h1>
           <p className="text-lg text-[#c59f61]">
-            We'd love to hear from you! Please fill out the form below or use our contact details.
+            We&apos;d love to hear from you! Please fill out the form below or use our contact details.
           </p>
         </div>
 
